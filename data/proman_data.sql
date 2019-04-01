0d47969339ddaa56fb1c701233c15d8d449b8753
Andras Molnar <andris9016@gmail.com>
2019-04-01 14:30:44 +0200

    Add sample datas to database

--- data/proman_data.sql
+++ data/proman_data.sql
@@ -2,3 +2,68 @@ ALTER TABLE IF EXISTS ONLY public.boards DROP CONSTRAINT IF EXISTS pk_boards_id 
 ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS pk_cards_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.statuses DROP CONSTRAINT IF EXISTS pk_statuses_id CASCADE;
 
+
+DROP TABLE IF EXISTS public.boards CASCADE;
+DROP SEQUENCE IF EXISTS public.boards_id_seq;
+CREATE TABLE boards (
+  id serial NOT NULL,
+  title text,
+PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS public.cards CASCADE;
+DROP SEQUENCE IF EXISTS public.cards_id_seq;
+CREATE TABLE cards (
+  id serial NOT NULL,
+  board_id integer,
+  title text,
+  statuses_id integer,
+  card_order integer,
+PRIMARY KEY (id)
+);
+
+
+DROP TABLE IF EXISTS public.statuses CASCADE;
+DROP SEQUENCE IF EXISTS public.statuses_id_seq;
+CREATE TABLE statuses (
+  id serial NOT NULL,
+  title text,
+PRIMARY KEY (id)
+);
+
+
+INSERT INTO boards VALUES (1, 'Board 1');
+INSERT INTO boards VALUES (2, 'Board 2');
+SELECT pg_catalog.setval('boards_id_seq', 2, true);
+
+
+INSERT INTO cards VALUES (1, 1, 'new card 1', 1, 0);
+INSERT INTO cards VALUES (2, 1, 'new card 1', 1, 1);
+INSERT INTO cards VALUES (3, 1, 'in progress card', 2, 0);
+INSERT INTO cards VALUES (4, 1, 'planning', 3, 0);
+INSERT INTO cards VALUES (5, 1, 'done card 1', 4, 0);
+INSERT INTO cards VALUES (6, 1, 'done card 1', 4, 1);
+INSERT INTO cards VALUES (7, 2, 'new card 1', 1, 0);
+INSERT INTO cards VALUES (8, 2, 'new card 2', 1, 1);
+INSERT INTO cards VALUES (9, 2, 'in progress card', 2, 0);
+INSERT INTO cards VALUES (10, 2, 'planning', 3, 0);
+INSERT INTO cards VALUES (11, 2, 'done card 1', 4, 0);
+INSERT INTO cards VALUES (12, 2, 'new card 1', 4, 1);
+SELECT pg_catalog.setval('cards_id_seq', 12, true);
+
+
+INSERT INTO statuses VALUES (1, 'new');
+INSERT INTO statuses VALUES (2, 'in progress');
+INSERT INTO statuses VALUES (3, 'testing');
+INSERT INTO statuses VALUES (4, 'done');
+SELECT pg_catalog.setval('statuses_id_seq', 4, true);
+
+
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT pk_boards_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE;
+
+
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT pk_statuses_id FOREIGN KEY (statuses_id) REFERENCES statuses(id) ON DELETE CASCADE;
+
