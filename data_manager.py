6e90b183222bb55d02d2d3c09e33573a2df89458
Andras Molnar <andris9016@gmail.com>
2019-04-01 20:50:02 +0200

    Get boards implemented with database

---
+++ data_manager.py
@@ -0,0 +1,17 @@
+import connection
+
+
+@connection.connection_handler
+def get_boards(cursor):
+    cursor.execute("""SELECT * FROM boards;""")
+
+    boards = cursor.fetchall()
+    return boards
+
+
+@connection.connection_handler
+def get_cards_for_board(cursor, board_id):
+    cursor.execute("""SELECT * FROM cards WHERE board_id=%(board_id)s;""", {"board_id": board_id})
+
+    cards = cursor.fetchall()
+    return cards
