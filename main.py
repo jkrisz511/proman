c791095fda60948339b2dda6761e3ae0bae47dd6
Andras Molnar <andris9016@gmail.com>
2019-04-17 21:25:06 +0200

    Refactor python file pep8 type

--- main.py
+++ main.py
@@ -70,6 +70,7 @@ def rename_column(column_id):
     new_title = request.get_json()['title']
     return data_manager.update_column(column_id, new_title)
 
+
 @app.route("/delete-card-/<int:card_id>", methods=['POST'])
 @json_response
 def delete_card(card_id):
